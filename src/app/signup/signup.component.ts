54abc7f566c623610b2055e32bdd21b8acdae084
Louis Huang <0563louishuang@gmail.com>
2017-02-09 20:48:18 -0800

    sign up form front end validation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,44 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Validators, FormBuilder, FormGroup, FormControl } from "@angular/forms";
 import { User } from "../model/user.model";
 import { AuthService } from "../service/authentication.service";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: 'signup.component.html',
-  styleUrls: ['signup.component.sass']
+    selector: 'app-signup',
+    templateUrl: 'signup.component.html',
+    styleUrls: [ 'signup.component.sass' ]
 })
+
 export class SignupComponent implements OnInit {
 
-  signUpForm: FormGroup;
+    signUpForm: FormGroup;
 
-  constructor(private authService: AuthService) { }
+    constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
 
-  onSubmit() {
-    const user = new User(
-        this.signUpForm.value.email,
-        this.signUpForm.value.password,
-        this.signUpForm.value.firstName,
-        this.signUpForm.value.lastName
-    );
-    this.authService.signup(user)
-        .subscribe(
-            data => console.log(data),
-            error => console.error(error)
+    ngOnInit() {
+        this.signUpForm = this.formBuilder.group({
+            'firstName': [ '', [
+                Validators.required,
+                Validators.minLength(2),
+                Validators.maxLength(24)  ]
+            ],
+            'lastName': [ '', [
+                Validators.required,
+                Validators.minLength(2),
+                Validators.maxLength(24)  ]
+            ],
+            'email': [ '', [
+                Validators.required,
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}" +
+                    "~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9]" +
+                    "(?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?") ]
+            ],
+            'password': [ '', [
+                Validators.required,
+                Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/) ]
+            ]
+
+        });
+
+    }
+
+    onSubmit() {
+        const user = new User(
+            this.signUpForm.value.email,
+            this.signUpForm.value.password,
+            this.signUpForm.value.firstName,
+            this.signUpForm.value.lastName
         );
-    this.signUpForm.reset();
-  }
+        this.authService.signup(user)
+            .subscribe(
+                data => console.log(data),
+                error => console.error(error)
+            );
+        this.signUpForm.reset();
+    }
 
-  ngOnInit() {
-    this.signUpForm = new FormGroup({
-      firstName: new FormControl(null, Validators.required),
-      lastName: new FormControl(null, Validators.required),
-      email: new FormControl(null, [
-          Validators.required,
-          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-      ]),
-      password: new FormControl(null, Validators.required)
-    });
-  }
+
+
+
+
+
 
 }
