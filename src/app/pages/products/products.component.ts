4ffaf386d964ace8eba82a4facc09e774ac9424f
Louis Huang <0563louishuang@gmail.com>
2017-03-21 21:03:41 -0700

    Add to cart form submit set up

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,24 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Product } from "../../models/product.model";
+import { Http } from "@angular/http";
+import { ProductService } from "../../service/product.service";
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.sass']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: [ './products.component.sass' ]
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+    constructor(private http: Http, private productService: ProductService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  sizeCharts: string[] = [
-      'Small',
-      'Medium',
-      'Large',
-      'X-Large',
-      'XX-Large',
-      'XXX-Large'
-  ];
+    sizeCharts: string[] = [
+        'Small',
+        'Medium',
+        'Large',
+        'X-Large',
+        'XX-Large',
+        'XXX-Large'
+    ];
+
+    addToCart(form: NgForm) {
+        const product = new Product(
+            form.value.size,
+            form.value.quantity
+        );
+        this.productService.addProductToCart(product)
+            .subscribe(
+                data => console.log(data),
+                error => console.error(error)
+            );
+        form.resetForm();
+    }
 
 }
