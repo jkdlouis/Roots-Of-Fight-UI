b69fd246b9817a1852b53f763d9f7ea9f8e39c8e
Louis Huang <0563louishuang@gmail.com>
2017-03-21 21:02:04 -0700

    adding product model

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,10 @@
+export class Product {
+
+    constructor(public size?: string,
+                public quantity?: number,
+                public title?: string,
+                public price?: string,
+                public productId?: string,
+                public userId?: string) {
+    }
+}
